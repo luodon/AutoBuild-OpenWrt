d2596b75794f4ac0bd1720caae3971cbcc549d26
luodon <74279058+luodon@users.noreply.github.com>
2021-12-31 18:41:54 +0800

    Update diy-part.sh

--- build/openwrt_amlogic/diy-part.sh
+++ build/openwrt_amlogic/diy-part.sh
@@ -3,8 +3,8 @@
 # DIY扩展二合一了，在此处可以增加插件
 
 git clone https://github.com/gd0772/package package/gd772
-wget https://raw.githubusercontent.com/gd0772/patch/main/n1.sh
-bash n1.sh
+# wget https://raw.githubusercontent.com/gd0772/patch/main/n1.sh
+# bash n1.sh
 
 # 设置打包固件的机型，内核组合（可用内核是时时变化的,过老的内核就删除的，所以要选择什么内核请看说明)
 cat >$GITHUB_WORKSPACE/amlogic_openwrt <<-EOF
